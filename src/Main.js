1f7d96cd367d8895e0a5fe4110f36e1a2c18902c
vhoangssf <vincenthoang00@gmail.com>
2017-11-30 13:20:58 -0800

    Created a unit test and installed dependencies karma and mocha

--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
 
 class Main extends React.Component {
+
   render() {
     return (
       <div>
         Hello world from Main.js
-        <Button bsSize="large">Info</Button>
       </div>
     );
   }
